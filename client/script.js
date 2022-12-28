0f4f48e0b2b96e069dc0921a9c19094adcd9a3ca
Marek Mamontowicz <kyltymzygzym@gmail.com>
2022-12-28 15:54:37 +0100

    'update'

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from serwer odpowiedz bota
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://aychcodex.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
